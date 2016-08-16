3875abc9fd5567a200325a2f898bcf78d2ea77d5
nhuckle <nhuckle@hotmail.com>
2016-08-16 15:23:41 -0400

    16/08/16 Changes
    Nouveau Matériel, Enlever le commentaire //suck my dick

--- Scripts/staticVariables.js
+++ Scripts/staticVariables.js
@@ -1,8 +1,8 @@
-﻿#pragma strict
+#pragma strict
 
 public static var baseCharSpeed:float = 5;
 public static var baseCharMaxSpeed:float = 10;
-public static var baseCharSprintMod:float = 1.5;
+public static var baseCharSprintMod:float = 1.7;
 public static var baseGravity:float = 9.8;
 
 function Start () {
