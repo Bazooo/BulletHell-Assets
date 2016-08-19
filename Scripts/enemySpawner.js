7afa95346f8e8cc2a69b1071282860d36d78863f
Mathieu Chan <mathieu77197@gmail.com>
2016-08-19 14:02:18 -0400

    Enemy Spawner (Now spawns enemies)
    Instantiates from the "Resources/Enemy" folder

--- Scripts/enemySpawner.js
+++ Scripts/enemySpawner.js
@@ -1,13 +1,34 @@
 ﻿#pragma strict
 
 var waves: EnemyWave[];
+private var spawnerTime: float = 0;
+private var nextWaveTime:float = 0;
+private var spawnerWave: int = 0;
 
 public class EnemyWave {
+    // var name: String = "";
     var name: Enemy = staticVariables.enemyBank;
     var amount: int = 1;
     var delay: float = 0.0;
 }
 
 public function Awake() {
+    nextWaveTime = waves[0].delay;
+}
 
+public function FixedUpdate() {
+    spawnerTime += Time.deltaTime;
+    if(spawnerTime >= nextWaveTime) {
+        var wave = waves[spawnerWave];
+        for (var i = 0; i < wave.amount; i++) {
+            Instantiate(Resources.Load("Enemy/" + wave.name));
+        }
+        spawnerWave++;
+        if(spawnerWave == waves.length) {
+            GetComponent(enemySpawner).enabled = false;
+            return;
+        }
+
+        nextWaveTime = spawnerTime + waves[spawnerWave].delay;
+    }
 }
